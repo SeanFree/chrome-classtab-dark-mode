badeaf96b70971572c7054e74c335abdcb541797
Sean Free <seanalexanderfree@gmail.com>
2019-12-08 19:41:47 -0500

    Updated background and popup scripts, added content script, configured popup functionality

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,15 @@
+const extKey = 'ctActiveTheme'
+const defaultTheme = 'dark-base'
+
+let activeTheme
+
 chrome.runtime.onInstalled.addListener(function () {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
 		chrome.declarativeContent.onPageChanged.addRules([
 			{
 				conditions: [
 					new chrome.declarativeContent.PageStateMatcher({
-						pageUrl: {hostEquals: 'www.classtab.org'},
+						pageUrl: { hostEquals: 'www.classtab.org' },
 					})
 				],
 				actions: [new chrome.declarativeContent.ShowPageAction()]
